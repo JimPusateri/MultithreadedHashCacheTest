06b5ee069deb1dd2c34af5059971c21386987402
Jim Pusateri <james.pusateri@middlecoastsoftware.com>
2022-09-10 10:01:30 -0400

    confirmation of single threaded cache integrity
    cache generator and dictionary based confirmation of unique response generation

--- src/MultithreadedHashCacheTest/MultithreadedHashCacheTest/Program.cs
+++ src/MultithreadedHashCacheTest/MultithreadedHashCacheTest/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.Caching;
+
+
 Console.WriteLine("Hello, World!");
+MemoryCache _memCache = MemoryCache.Default;
+
+var sleepTimeSpan = new TimeSpan(1000);
+Dictionary<string, object> _cache = new Dictionary<string, object>();
+
+for(int i = 0; i<5000000;i++)
+{
+    var timeStamp = GetCacheSafeTimeStamp();
+    _cache.Add(timeStamp, new object());
+}
+
+Console.WriteLine(_cache.Keys.Count);
+
+
+string GetCacheSafeTimeStamp()
+{
+    var dt = DateTime.Now;
+    var ts = dt.ToFileTimeUtc().ToString();
+    while (_memCache.Contains(ts))
+    {
+        Thread.Sleep(sleepTimeSpan);
+        ts = dt.Add(sleepTimeSpan).ToFileTimeUtc().ToString();
+    }
+    _memCache.Add(ts, ts, DateTime.UtcNow.AddMilliseconds(220));
+    return ts;
+}
